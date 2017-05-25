22b4b0eb33c5625bb65aebfb39eca93107c75cfb
Ken Rutan and Sean Pierce <sean@>
2017-05-25 14:50:46 -0700

    Add styles to map

--- build/js/app.js
+++ build/js/app.js
@@ -1,10 +1,211 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 function initMap(new_spots, zoom) {
+
+  var styledMapType = new google.maps.StyledMapType(
+    [
+      {
+        "featureType": "all",
+        "elementType": "all",
+        "stylers": [
+          {
+            "invert_lightness": true
+          },
+          {
+            "saturation": 10
+          },
+          {
+            "lightness": 30
+          },
+          {
+            "gamma": 0.5
+          },
+          {
+            "hue": "#435158"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative.locality",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+              "color": "#ccbca2"
+            }
+        ]
+      },
+      {
+        "featureType": "administrative.neighborhood",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#ccbca2"
+          }
+        ]
+      },
+      {
+      "featureType": "administrative.land_parcel",
+      "elementType": "all",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
+      },
+      {
+      "featureType": "landscape.man_made",
+      "elementType": "geometry.fill",
+      "stylers": [
+          {
+              "visibility": "on"
+          }
+      ]
+      },
+      {
+      "featureType": "poi",
+      "elementType": "all",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
+      },
+      {
+      "featureType": "poi",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
+      },
+      {
+      "featureType": "poi",
+      "elementType": "labels.text.fill",
+      "stylers": [
+          {
+              "color": "#a39b8f"
+          }
+      ]
+      },
+      {
+      "featureType": "poi.attraction",
+      "elementType": "all",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
+      },
+      {
+      "featureType": "poi.business",
+      "elementType": "all",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
+      },
+      {
+      "featureType": "poi.government",
+      "elementType": "all",
+      "stylers": [
+          {
+              "visibility": "simplified"
+          }
+      ]
+      },
+      {
+      "featureType": "poi.park",
+      "elementType": "geometry.fill",
+      "stylers": [
+          {
+              "color": "#1c2833"
+          }
+      ]
+      },
+      {
+      "featureType": "road.highway",
+      "elementType": "geometry.fill",
+      "stylers": [
+          {
+              "color": "#4f5858"
+          }
+      ]
+      },
+      {
+      "featureType": "road.highway",
+      "elementType": "geometry.stroke",
+      "stylers": [
+          {
+              "color": "#3a3d3d"
+          }
+      ]
+      },
+      {
+      "featureType": "road.highway",
+      "elementType": "labels.text.fill",
+      "stylers": [
+          {
+              "color": "#a39b8f"
+          }
+      ]
+      },
+      {
+      "featureType": "transit.line",
+      "elementType": "geometry.fill",
+      "stylers": [
+          {
+              "visibility": "on"
+          },
+          {
+              "color": "#814c3a"
+          }
+      ]
+      },
+      {
+      "featureType": "transit.line",
+      "elementType": "labels",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
+      },
+      {
+      "featureType": "water",
+      "elementType": "geometry.fill",
+      "stylers": [
+          {
+              "color": "#0f1820"
+          }
+      ]
+      },
+      {
+      "featureType": "water",
+      "elementType": "labels.text.fill",
+      "stylers": [
+          {
+              "color": "#1d496a"
+          }
+      ]
+      }
+      ],
+            {name: 'Styled Map'});
+
+
   var last_spot = new_spots[new_spots.length-1];
   var map = new google.maps.Map(document.getElementById('map'), {
+    // mapTypeId: 'hybrid',
     zoom: zoom,
-    center: last_spot.info
+    center: last_spot.info,
+    mapTypeControlOptions: {
+      mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
+              'styled_map']
+    }
   });
+
+  map.mapTypes.set('styled_map', styledMapType);
+  map.setMapTypeId('styled_map');
 
   new_spots.forEach(function(spot, i) {
     var marker = new google.maps.Marker({
@@ -31,7 +232,7 @@ function printLocations(new_spots) {
 // });
 
 
-var new_spots = [{info:{lat: 45.52, lng: -122.68}, query:"Epicodus"}];
+var new_spots = [{info:{lat: 45.52, lng: -122.68}, query:"You"}];
 
 $(document).ready(function() {
 
